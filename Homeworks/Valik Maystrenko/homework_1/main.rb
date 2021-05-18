69932d8acb0909569b2292de3c5717087c341cb6
blackwolf3342 <valintin.maystrenko1998@gmail.com>
2021-05-18 20:01:33 +0300

    Update

--- Homeworks/Valik Maystrenko/homework_1/main.rb
+++ Homeworks/Valik Maystrenko/homework_1/main.rb
@@ -1,21 +1,12 @@
-names   = %w[Vlad Ihor Anatolii Alex Yana Iren Ksenia]
+names = %w[Vlad Ihor Anatolii Alex Yana Iren Ksenia]
 hobbies = %i[volleyball football reading swimming climbing]
-
 users = names.map {|name| { name: name, age: rand(7..78), hobbies: hobbies.sample(rand(1..3)) } }
-
 users_age = []
-
 count_hobbies = Hash.new(0)
-
 users = users.map! do |user|
   user[:adult] = user[:age] >= 18
-
   if user[:hobbies].length < 2
-    if user[:hobbies].include?(:football) 
-      user[:hobbies].push(:reading) 
-    else 
-      user[:hobbies].push(:football)
-    end
+    user[:hobbies].include?(:football) ? user[:hobbies].push(:reading) : user[:hobbies].push(:football)
   end
   user[:hobbies].each { |hobbie| count_hobbies[hobbie] += 1}
   users_age.push(user[:age])
@@ -26,13 +17,4 @@ list_of_popular_hobbies = (1..3).map { |i| count_hobbies.keys[i] }
 oldest_user   = users.find {|u| u[:age] == users_age.max }
 avg_age       = users_age.inject(0) { |sum, el| sum + el} / users_age.size
 adult_users   = users.find_all{ |user| user[:adult]}.map{|u| u[:name] }
-
-puts %{
-  Names of adult users: #{adult_users}
-  List of top 3 popular hobbies: #{list_of_popular_hobbies}
-  Average user age: #{avg_age}
-  Info about an oldest user:
-    name: #{oldest_user[:name]}
-    age: #{oldest_user[:age]}
-    hobbies: #{oldest_user[:hobbies]}
-}
+puts "Names of adult users: #{adult_users}\nList of top 3 popular hobbies: #{list_of_popular_hobbies}\nAverage user age: #{avg_age} \nInfo about an oldest user:\n\t name: #{oldest_user[:name]} \n\t age: #{oldest_user[:age]} \n\t hobbies: #{oldest_user[:hobbies]}"
