11c33790e69bb1f675b22f636fc32e4e4535acae
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-05-21 16:55:31 +0300

    fixed adding new hobbie for user

--- Homeworks/Andrii_Tsvirko/homework_1/hw-1.rb
+++ Homeworks/Andrii_Tsvirko/homework_1/hw-1.rb
@@ -8,11 +8,11 @@ users.each do |user|
   #  Adding key adult for all users
   user[:adult] = user[:age] >= 18
 
-  # Всім, в кого менше 2х хобі, потрібно додати хоббі  football . Якщо таке хоббі вже є, додати любе нове для обраного юзера хоббі
-  unique_hobbies = (user[:hobbies] | hobbies)
+  # Всім, в кого менше 2х хобі, потрібно додати хоббі  football .Якщо таке хоббі вже є, додати любе нове для обраного юзера хоббі
   next unless user[:hobbies].size < 3
-  new_hobbie = user[:hobbies].include?(:football) ? unique_hobbies.sample : :football
-  user[:hobbies] << new_hobbie 
+    unique_hobbies = hobbies - user[:hobbies]
+    new_hobbie = user[:hobbies].include?(:football) ? unique_hobbies.sample : :football
+    user[:hobbies] << new_hobbie 
 end
 
 # Names of adult users: ....
