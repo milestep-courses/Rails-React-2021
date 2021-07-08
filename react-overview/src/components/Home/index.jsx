15c767651463faf5c0b484f73060b4b3bb1220ca
Ripchanskiy <sergeyripchanskiy@gmail.com>
2021-07-08 18:13:37 +0300

    hooks and map

--- react-overview/src/components/Home/index.jsx
+++ react-overview/src/components/Home/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.sass';
 
 export const Home = () => {
+  const [posts, setPosts] = useState([]);
+
   console.log('Home component render');
-  return <div className={styles.home}>Home component</div>;
+
+  const onFetchPosts = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts));
+  };
+
+  console.log(posts);
+  useEffect(() => {
+    onFetchPosts();
+  }, []);
+  return (
+    <div className={styles.home}>
+      Home component
+      <ul>
+        {posts.slice(0, 10).map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
--- react-overview/src/components/Welcome/index.jsx
+++ react-overview/src/components/Welcome/index.jsx
@@ -7,7 +7,7 @@ class Welcome extends Component {
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 3000);
+    this.timerID = setInterval(() => this.tick(), 2000);
   }
 
   componentWillUnmount() {
