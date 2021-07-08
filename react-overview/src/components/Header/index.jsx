490762d35a10751a150904425e51c013568ef743
Ripchanskiy <sergeyripchanskiy@gmail.com>
2021-07-08 17:27:42 +0300

    basic structure, eslint, pretier

---
+++ react-overview/src/components/Header/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.sass';
+
+export const Header = () => {
+  return <div className={styles.home}>Header component</div>;
+};
---
+++ react-overview/src/components/Home/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.sass';
+
+export const Home = () => {
+  return <div className={styles.home}>Home component</div>;
+};
