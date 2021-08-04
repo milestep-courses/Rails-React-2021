d71b2ac2ce0be16bada91b1b7d1cfc737cd4cae1
Ripchanskiy <sergeyripchanskiy@gmail.com>
2021-08-04 10:28:39 +0300

    useMemo

--- react-overview/src/components/Footer/index.jsx
+++ react-overview/src/components/Footer/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './styles.module.sass';
 import { useAuth } from '../../context/AuthContext';
 
 export const Footer = () => {
   const { increment } = useAuth();
-  console.log('Footer component render');
-  return (
-    <div className={styles.footer}>
-      Footer component<button onClick={increment}>Increment Value</button>
-    </div>
-  );
+  return useMemo(() => {
+    console.log('Footer component render');
+    return (
+      <div className={styles.footer}>
+        Footer component
+        <button onClick={increment}>Increment Value</button>
+      </div>
+    );
+  }, []);
 };
